0277fe420b313b64adfb83535f82adce90d1865e
Kennedy Marsh <kikimarsholl@gmail.com>
2020-02-27 23:09:31 -0500

    Level 2 complete
    Created RosterGenerator class

--- untitled/src/RosterGenerator.java
+++ untitled/src/RosterGenerator.java
@@ -2,12 +2,11 @@ import java.util.*;
 import java.io.*;
 
 public class RosterGenerator{
-    //private Player[] roster;
+    private static Player[] roster;
 
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        Player[] roster = new Player[5];
-        
+        generateTeam();
     }
 
 //    public void replacePlayer(){
@@ -22,11 +21,29 @@ public class RosterGenerator{
 //
 //    }
 //
-//    public void generateTeam(){
-//
-//    }
-//
-//    public void outputRoster(){
-//
-//    }
+    public static void generateTeam(){
+        Scanner scan = new Scanner(System.in);
+        roster = new Player[5];
+        int userJersey = -1;
+        int userRating = -1;
+        for(int i = 0; i < 5; i++){
+            while(userJersey < 0 || userJersey > 99){
+                System.out.println("Enter player " + (i+1) + "'s jersey number:");
+                userJersey = scan.nextInt();
+                if(userJersey < 0 || userJersey > 99){
+                    System.out.println("Invalid jersey number. Please try again.");
+                }
+            }
+            while(userRating < 1 || userRating > 9){
+                System.out.println("Enter player " + (i+1) + "'s rating:");
+                userRating = scan.nextInt();
+                if(userRating < 1 || userRating > 9){
+                    System.out.println("Invalid player rating. Please try again.");
+                }
+                System.out.println("");
+            }
+            Player p = new Player(userJersey, userRating);
+            roster[i] = p;
+        }
+    }
 }
